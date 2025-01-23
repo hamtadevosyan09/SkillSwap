295d6522ef8e27bf8109d3f5dbb0b116494dd6af
hamlettadevosyan <tadevosyanhamlet09@gmail.com>
2025-01-23 17:11:23 +0400

    login

---
+++ app/src/main/java/com/myproject/skillswap/CreatePostFragment.java
@@ -0,0 +1,18 @@
+package com.myproject.skillswap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreatePostFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_post, container, false);
+    }
+}
